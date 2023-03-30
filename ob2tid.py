52bd8b42bdeb1dfe2c4541001271255d4d0c406e
bie-zheng <99402968+bie-zheng@users.noreply.github.com>
2023-03-30 23:25:56 +0800

    ob2tid1

---
+++ ob2tid.py
@@ -0,0 +1,76 @@
+import os
+import datetime
+
+# 读取md文件内容
+
+def extract_code(file_path):
+    with open(file_path, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+    in_code_block = False
+    for i in range(len(lines)):
+        line = lines[i]
+
+        # check if in code block
+        if line.startswith('```'):
+            in_code_block = not in_code_block
+
+        # process headers
+        if not in_code_block and line.startswith('#'):
+            level = line.count('#')
+            lines[i] = '!' * level + line[level:]
+
+        # process unordered lists
+        if not in_code_block and line.startswith('-'):
+            lines[i] = '#' + line[1:]
+
+        # process ordered lists
+        if not in_code_block and line[0].isdigit() and line[1] == '.':
+            lines[i] = '*' + line[1:]
+    return ''.join(lines)
+
+def add_head(file_path,tags) :
+    # 获取不带后缀的文件名
+    basename = os.path.splitext(file_path)[0]
+    result = []
+    for s in basename.split('\\'):
+        for ss in s.split('/'):
+            result.append(ss)
+    basename = result[-1]
+    # 获取创建和修改日期
+    now = datetime.datetime.now()
+    timestamp = str(now.year) + str(now.month).zfill(2) + str(now.day).zfill(2) + str(now.hour).zfill(2) + str(
+        now.minute).zfill(2) + str(now.second).zfill(2) + str(now.microsecond).zfill(6)
+    # 输出结果
+    title="title:"+basename
+    creat="created:"+str(timestamp)
+    modified="modified:"+str(timestamp)
+    return creat+'\n'+modified+'\n'+'tags:'+tags+'\n'+"title:"+basename+"\n"+"type: text/vnd.tiddlywiki"+"\n\n"
+
+def allinone(file_path,tags):
+    output=add_head(file_path,tags)+extract_code(file_path)
+#    print(output)
+    basename = os.path.splitext(file_path)[0]
+    result = []
+    for s in basename.split('\\'):
+        for ss in s.split('/'):
+            result.append(ss)
+    basename = result[-1]
+    print(basename)
+    filename=str(basename)+".tid"
+#    os.makedirs("C:/Users/别正/Desktop/临时") 
+#如果没有，新建此文件夹
+    with open("C:/Users/别正/Desktop/临时/"+filename, 'w', encoding='utf-8') as f:
+        f.write(output)
+
+def process_folder(folder_path):
+    for file_name in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, file_name)
+        if os.path.isdir(file_path):
+            process_folder(file_path)
+        elif file_name.endswith(".md"):
+            tags = os.path.basename(folder_path)
+            allinone(file_path, tags)
+
+#allinone('example.md','love')
+process_folder("C:/Users/别正/Desktop/ob文件库")
+
